d679c9dbb43135039a8f37e9ea473e01409cf070
Jonathan Corley <jcorley@noscihigh.org>
2021-03-01 16:51:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,93 @@
+/**
+ * IN CLASS EXERCISE: LOOPS
+ */
+
+/** 
+ * Given an input Array, loop forward over the Array and print its values 
+ * using console.log().
+ */
+function printArrayValues(array) {
+  // YOUR CODE BELOW HERE //
+  
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+
+
+
+
+
+
+/** 
+ * Given an input Array, loop backwards over the Array and print its values 
+ * using console.log().
+ */
+function printArrayValuesInReverse(array) {
+  // YOUR CODE BELOW HERE //
+
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+
+
+
+
+
+/** 
+ * Given an input Object, return an Array containing the Object keys.
+ */
+function getObjectKeys(object) {
+  // YOUR CODE BELOW HERE //
+     
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+
+
+
+
+/** 
+ * Given an input Object, loop over the Object and print its keys 
+ * using console.log().
+ */
+function printObjectKeys(object) {
+  // YOUR CODE BELOW HERE //
+  
+
+  // YOUR CODE ABOVE HERE //
+}
+
+
+
+
+
+/** 
+ * Given an input Object, return an Array containing the Object's values.
+ */
+function getObjectValues(object) {
+  // YOUR CODE BELOW HERE //
+  
+  
+  // YOUR CODE ABOVE HERE //
+}
+
+
+
+
+
+
+
+/** 
+ * Given an input Object, loop over the Object and print its values 
+ * using console.log().
+ */
+function printObjectValues(object) {
+  // YOUR CODE BELOW HERE //
+
+  
+  // YOUR CODE ABOVE HERE //
+}
+
