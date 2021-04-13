b0ab90e2d255469bac8fd43d7e7e62c55cf72c8c
Shelyn Fuentes <>
2021-04-13 19:56:26 +0000

    .

--- index.js
+++ index.js
@@ -1,94 +1,93 @@
 /**
  * IN CLASS EXERCISE: LOOPS
  */
+var myObj} = {
+  one: 1,
+    two: 2,
+      three: 3,
+        /**
+         * Given an input Array, loop forward over the Array and print its values 
+         * using console.log().
+         */
+        function printArrayValues(array) {
+          // YOUR CODE BELOW HERE //
 
-/** 
- * Given an input Array, loop forward over the Array and print its values 
- * using console.log().
- */
-function printArrayValues(array) {
-  // YOUR CODE BELOW HERE //
+          var myArray = [1, 2, 3, 4, 5]
+          // YOUR CODE ABOVE HERE //
+        }
+
+
+  /** 
+   * Given an input Array, loop backwards over the Array and print its values 
+   * using console.log().
+   */
+  function printArrayValuesInReverse(array) {
+    // YOUR CODE BELOW HERE //
+
+    //iterate backwards over array
+    for (let i = array.length -; 1 >= 0; i--)()
+    console.log = (array[i])
+    // YOUR CODE ABOVE HERE //
+  }
+
+
+
+
+
+  /** 
+   * Given an input Object, return an Array containing the Object keys.
+   *
+  function getObjectKeys(object) {
+    // YOUR CODE BELOW HERE //
+      function getObjectKeys(object) {
+        var myObj = (myObj)
+    console.log
+    // YOUR CODE ABOVE HERE //
+  }
   
   
-  // YOUR CODE ABOVE HERE //
-}
-
-
-
-
-
-
-
-
-/** 
- * Given an input Array, loop backwards over the Array and print its values 
- * using console.log().
- */
-function printArrayValuesInReverse(array) {
-  // YOUR CODE BELOW HERE //
-
   
-  // YOUR CODE ABOVE HERE //
-}
+  /** 
+   * Given an input Object, loop over the Object and print its keys 
+   * using console.log().
+   */
+  function printObjectKeys(object) {
+    // YOUR CODE BELOW HERE //
+//iterate over the object
+for(var key in object)(
+  //print the keys
+  console.log(key)
+
+    // YOUR CODE ABOVE HERE //
+  }
 
 
 
 
 
+  /** 
+   * Given an input Object, return an Array containing the Object's values.
+   */
+  function getObjectValues(object) {
+    // YOUR CODE BELOW HERE //
 
-/** 
- * Given an input Object, return an Array containing the Object keys.
- */
-function getObjectKeys(object) {
-  // YOUR CODE BELOW HERE //
-     
-  
-  // YOUR CODE ABOVE HERE //
-}
 
+    // YOUR CODE ABOVE HERE //
+  }
 
 
 
+  /** 
+   * Given an input Object, loop over the Object and print its values 
+   * using console.log().
+   */
+  function printObjectValues(object) {
+    // YOUR CODE BELOW HERE //
 
-/** 
- * Given an input Object, loop over the Object and print its keys 
- * using console.log().
- */
-function printObjectKeys(object) {
-  // YOUR CODE BELOW HERE //
-  
-
-  // YOUR CODE ABOVE HERE //
-}
-
-
-
-
-
-/** 
- * Given an input Object, return an Array containing the Object's values.
- */
-function getObjectValues(object) {
-  // YOUR CODE BELOW HERE //
-  
-  
-  // YOUR CODE ABOVE HERE //
-}
-
-
-
-
-
-
-
-/** 
- * Given an input Object, loop over the Object and print its values 
- * using console.log().
- */
-function printObjectValues(object) {
-  // YOUR CODE BELOW HERE //
-
-  
-  // YOUR CODE ABOVE HERE //
-}
+//iterate over the object
+for(var keys in object)(
+  //print the keys
+  console.log(object[key])
+    // YOUR CODE ABOVE HERE //
+  }
 
